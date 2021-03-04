c475e1467ddf9637b60d1e15039c8d45ca549d9b
SteinOv <stein-overtoom@live.nl>
2021-03-04 18:41:56 +0100

    Batch importing implemented

--- main.jl
+++ main.jl
@@ -1,16 +1,47 @@
 using MS_Import
+function main()
+	# filenames=["B039695.mzXML", "B039699.mzXML"]
+
+	folder = "P:/Git/bachelor_project"
+	mzxml_path = "data/mzxml"
+	pathin = joinpath(folder, mzxml_path)
+
+	spectra = batch_import(pathin)
+
+	spectra[2]
 
 
+end
 
-folder = "P:/Git/bachelor_project"
-mzxml_path = "data/mzxml"
-pathin = joinpath(folder, mzxml_path)
-# filenames=["B039695.mzXML", "B039699.mzXML"]
-filenames=["B039695.mzXML"]
 
-# mz_values,mz_int,t0,t_end,file_name,pathin,
-# msModel,msIonisation,msManufacturer,polarity,Rt=import_files_MS1(pathin,filenames) # Only MS1
+function batch_import(pathin)
+	"""Returns list containing all spectra in folder"""
+	mz_thresh = [0, 0]
+	Int_thresh = 0
+	files = readdir(pathin)
+	files_supported = zeros(length(files),1)
 
-chrom=import_files(pathin,filenames)
+	# Determine supported files
+	for i=1:length(files)
+		if lowercase(files[i][end-4:end]) == "mzxml" || lowercase(files[i][end-3:end]) == "cdf"
+			files_supported[i] = 1
+		end
+	end
 
+	# Save indices of supported files
+	supported_indices=findall(x -> x > 0, files_supported)
+	
+	# Create array for storing spectra
+	spectra = Array{Any}(undef,size(supported_indices,1))
+
+	# Load files into spectra array
+	for i in supported_indices
+		filenames = [files[i]]
+		spectra[i]=import_files(pathin,filenames,mz_thresh,Int_thresh)
+	end
+
+	return spectra
+end
+
+main()
 
