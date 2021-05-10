fe9014a98be4f5dba5d6f6484cf93268cdd4fd3b
SteinOv <stein-overtoom@live.nl>
2021-05-10 13:22:54 +0200

    Grouped functions in manual_visualization.jl and extra.jl into manual_inspection.jl

--- manual_inspection.jl
+++ manual_inspection.jl
@@ -1,11 +1,16 @@
 """
-Functions for visualising spectra manually.
+Functions for manual inspection of spectra.
 
 plt: Plots spectrum for chosen mz values and retention time
 plt!: Same as plt, but plots on top of previous plot
 mass_ratios: Prints mz values of chosen compounds and their (normalised) ratios
+display_distribution: Integrates all mz values of specific compound in spectrum and outputs to .csv file
+manual_integration: Manually integrates spectrum between chosen RT and mz values
 
 """
+
+include("main.jl")
+include("helpers.jl")
 
 function plt(mass=0, RT=0, RT_range_size=0.5)
 	"""Plot spectrum of given mass
@@ -121,3 +126,72 @@ function mass_ratios(compound, spectra=spectra, compounds=compounds)
         # end
     
     end
+
+
+	
+function display_distribution(compound)
+    data_folder = joinpath(@__DIR__, "data")
+    subfolder = "coca_caf_cal"
+    pathin = joinpath(data_folder, subfolder)
+    csvout = joinpath(pathin, "distribution.csv")
+
+    spectra = batch_import(pathin)
+
+    compounds = CSV.read("compounds.csv", DataFrame)
+    filter!(row -> !(any(ismissing, (row.RT, row.mz)) || any((row.RT, row.mz) .== 0)), compounds)
+
+    compound_row = compounds[findfirst(comp -> comp == compound, compounds.compound), :]
+    
+
+    # Read mz values
+    mass_str = split(compound_row.mz, ";")
+    mass_values = Array{Any,1}(undef, length(mass_str))
+    for (i, mass) in enumerate(mass_str)
+        if startswith(mass, "(")
+            mass = split(mass[2:end - 1], ",")
+            mass_values[i] = Tuple(([parse(Float32, sub_mass) for sub_mass in mass]))
+        else
+            mass_values[i] = parse(Float32, mass)
+        end	
+    end
+
+    distribution = DataFrame()
+    insertcols!(distribution, :item_number => String[])
+    insertcols!(distribution, :filename => String[])
+    insertcols!(distribution, :folder => String[])
+    for mz in mass_values
+        insertcols!(distribution, Symbol(mz) => Int32[])
+    end
+
+    for i=1:length(spectra)
+        println("Analysing spectrum $i...")
+        spectrum = spectra[i]["MS1"]
+
+        RT_modifier = process_major(compounds, spectrum)[2]
+        RT = compound_row.RT * RT_modifier
+
+        # Save metadata
+        sample_metadata = Array{Any,1}(undef, 3)
+        sample_metadata[1] = spectrum["Sample Name"][1]
+        sample_metadata[2] = spectrum["Filename"][1]
+        sample_metadata[3] = subfolder
+
+        # Integrate peaks
+        mass_integral = integrate_peaks(spectrum, RT, mass_values)
+        push!(distribution, append!(sample_metadata, zeros(length(mass_values))))
+        for j=1:length(mass_values)
+            distribution[i, Symbol(string(mass_integral[j, 1]))] = mass_integral[j, 2]
+        end
+
+    end
+
+    CSV.write(csvout, distribution)
+end 
+
+function manual_integration(spectrum, RT_range, mz_values)
+    "Manually integrate spectrum between RT range at chosen mz values"
+    index_left, index_right = RT_indices(spectrum, RT_range)
+    spectrum_XIC = filter_XIC(spectrum, mz_values)
+
+    return sum(spectrum_XIC[index_left : index_right])
+end
