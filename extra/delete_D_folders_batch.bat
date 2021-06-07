c11ec0bcd3ebf41aeeb6c894158cd54d3cf866d3
SteinOv <stein-overtoom@live.nl>
2021-06-07 18:41:24 +0200

    Added .bat file for deleting .D folders in batch

---
+++ extra/delete_D_folders_batch.bat
@@ -0,0 +1,26 @@
+@REM Deletes all .D folders in all subdirectories of current directory
+
+@ECHO OFF
+setlocal EnableDelayedExpansion
+
+
+:PROMPT
+SET /P AREYOUSURE=This will delete all .D folders in all subdirectories, this cannot be reversed, are you sure? ([Y]/[N])?
+IF /I "%AREYOUSURE%" NEQ "Y" (
+    echo ------- Aborted Process -------
+    GOTO END
+)
+
+for /D %%d in (.\*) do (
+@echo ---------------------------Processing folder: %%~nd----------------------------
+
+    for /D %%D in (%%d\*) do (
+        set "folder_extension=%%~xD"
+        if !folder_extension!==.D RD /S /Q %%D
+        
+    )
+
+)
+:END
+
+pause
