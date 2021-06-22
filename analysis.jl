3254b3fa1aade454c5163905137742d6b9584ad7
SteinOv <stein-overtoom@live.nl>
2021-06-22 10:59:01 +0200

    Added function compound_trends
    Function compound_trends determines how often each compound is above the minimum value per month

--- analysis.jl
+++ analysis.jl
@@ -57,6 +57,51 @@ end
 # --------------------------------------------------------------------
 
 """
+    compound_trends(impurity_profile_csv, compound_group, month_range, minimum_value)
+Counts per month how many times each compound is above the minimum value
+Outputs this to monthly_data.csv in input folder
+"""
+function compound_trends(impurity_profile_csv, compound_group, minimum_value::Number)
+
+    # Read impurity profile and filter out control samples
+    impurity_profile = CSV.read(impurity_profile_csv, DataFrame)
+    filter!(x -> !contains(x["item_number"], "CTRL"), impurity_profile)
+
+    # Group impurity profiles by month
+    insertcols!(impurity_profile,1, :yearmonth => yearmonth.(map(x -> DateTime(x[1:end-1]), impurity_profile[!, "DateTime (y-m-d)"])))
+    impurity_profiles_grouped = groupby(impurity_profile, :yearmonth, sort=true)
+
+    # Read settings.json and retrieve compound names from group
+    json_string = read(joinpath(@__DIR__, "settings.json"), String)
+    settings_json = JSON3.read(json_string)
+    compound_names = settings_json[:compound_groups][compound_group]
+
+    # Create DataFrame for output
+    monthly_data = DataFrame()
+    insertcols!(monthly_data, :year => Int[])
+    insertcols!(monthly_data, :month => Int[])
+    for compound_name in compound_names
+		insertcols!(monthly_data, Symbol(compound_name) => Int32[])
+	end
+    insertcols!(monthly_data, :total_samples => Int[])
+
+    # Count times compounds above minimum value for each month
+    for group in impurity_profiles_grouped
+        total_samples = size(group, 1)
+        push!(monthly_data, append!([], group[1, :yearmonth], zeros(length(compound_names)), total_samples))
+        for compound_name in compound_names
+            monthly_data[end, compound_name] = count(i -> i >= minimum_value, group[!, compound_name])
+        end
+    end
+
+    CSV.write(joinpath(dirname(impurity_profile_csv), "monthly_data.csv"), monthly_data)
+
+end
+
+
+# --------------------------------------------------------------------
+
+"""
     find_similar_samples(sample, impurity_profile_csv, compound_group, max_deviation_fraction, date_range=0)
 Looks for similar samples in impurity profile, samples are determined as similar if for each compound: 
 {difference values / mean values <= max_deviation_fraction}
